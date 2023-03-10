eee34e0d360a9944ecf5f04ca83fcabb6f5fade0
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-03-11 01:38:18 +0300

    Remove postId from likes table

--- db/migrate/20230310014206_create_likes.rb
+++ db/migrate/20230310014206_create_likes.rb
@@ -1,7 +1,6 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :postId
       
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_022235) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_10_223620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_022235) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "postId"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
