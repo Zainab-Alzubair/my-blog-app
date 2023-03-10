d04e3e52775a8d18d9e7bb2bafba0bb4e2e31e32
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-03-11 02:37:51 +0300

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,10 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   def update_posts_counter
-  author.increment!(:postsCounter)
+    author.increment!(:postsCounter)
   end
 
   def recent_comments
-  comments.order(created_at: :desc).limit(5)
+    comments.order(created_at: :desc).limit(5)
   end
 end
