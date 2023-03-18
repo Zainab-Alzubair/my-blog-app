414adcfa3852e5267447676aa4a48bc26d630d1c
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-03-18 21:11:35 +0300

    Add like,comment and new post forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,7 @@ class Post < ApplicationRecord
     comments.order(created_at: :desc).limit(5)
   end
 
-  validates :title, presence: true, length: { minimum: 5, maximum: 250 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 250 }
   validates :commentsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likesCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
